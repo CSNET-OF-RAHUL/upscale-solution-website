5c4d5734812625308f43640fcbc848e1372e96d0
Rahul Verma <38274643+CSNET-OF-RAHUL@users.noreply.github.com>
2025-07-24 14:59:32 +0530

    Update layout.tsx

--- Upscale_Dev/app/terms-and-conditions/layout.tsx
+++ Upscale_Dev/app/terms-and-conditions/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../globals.css';
 import Header from '@/app/components/Header'; 
-import ScrollToTop from './components/ScrollToTop';
+import ScrollToTop from '../components/ScrollToTop';
 
 
 export default function TermsLayout({
