f08d155231674148ab95800067f221c7b02bcbb4
Rahul Verma <38274643+CSNET-OF-RAHUL@users.noreply.github.com>
2025-07-24 14:48:51 +0530

    Update layout.tsx

--- Upscale_Dev/app/layout.tsx
+++ Upscale_Dev/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
+import ScrollToTop from './components/ScrollToTop';
+
 
 const pacifico = Pacifico({
   weight: '400',
